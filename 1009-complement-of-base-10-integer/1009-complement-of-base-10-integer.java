9f7e63b5f2b6bb4243b4a620541fb39e841c467d
Gauri Agrawal <73654083+Gauri-Agrawal838@users.noreply.github.com>
2022-08-14 17:10:27 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (9.69%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -1,16 +1,20 @@
 class Solution {
     public int bitwiseComplement(int n) {
-        int mask = 0, m=n;
+        int res=0, power=1, bit;
         if(n==0)
             return 1;
-        while(m!=0)
+        while(n>0)
         {
-            mask = (mask << 1) | 1;
-            m = m >> 1;
-           
+            bit = (n&1);
+            if(bit==0)
+                res += power*1; 
+            n = n>>1;
+            power *= 2;
         }
-        int ans = (~n) & mask;
-        return ans;
+        return res;
     }
-    
 }
+
+// 5 = 1 0 1
+// if(bit==1)
+//     then it's complement will be equal to 0 and 0 will not contribute to answer
