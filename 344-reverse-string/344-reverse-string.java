286b2a3cfdf67168aef8038a1f949f736c1a9175
Gauri Agrawal <73654083+Gauri-Agrawal838@users.noreply.github.com>
2022-08-18 10:50:21 +0530

    Time: 1 ms (99.91%), Space: 58.2 MB (10.72%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -1,13 +1,11 @@
 class Solution {
     public void reverseString(char[] s) {
-        swap(s, 0, s.length-1);
-    }
-    public void swap(char[] s, int start, int end)
-    {
-        if (start >= end) return;
-        char temp = s[start];
-        s[start++] = s[end];
-        s[end--] = temp;
-        swap(s, start, end);
+        int left=0, right=s.length-1;
+        while(left<right)
+        {
+            char temp = s[right];
+            s[right--] = s[left];
+            s[left++] = temp;
+        }
     }
 }
