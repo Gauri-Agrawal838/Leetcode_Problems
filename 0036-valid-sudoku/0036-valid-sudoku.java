ede0d7cd578183cb15bc3a4e401fa51ed67262e3
Gauri Agrawal <73654083+Gauri-Agrawal838@users.noreply.github.com>
2023-03-16 08:57:58 +0530

    Time: 17 ms (12.72%), Space: 43.7 MB (12.20%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> seen = new HashSet<>();
+        //traverse the board row-wise & column-wise
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                char num = board[i][j];
+                if(num!='.')
+                {
+                    if(seen.contains(num + "_R_" + i)||
+                       seen.contains(num + "_C_" + j)||
+                       seen.contains(num + "_B_" + i/3 + "_" + j/3))
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        seen.add(num + "_R_" + i);
+                        seen.add(num + "_C_" + j);
+                        seen.add(num + "_B_" + i/3 + "_" + j/3);
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
