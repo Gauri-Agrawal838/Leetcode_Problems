c67a11e57800d9c59b0c14733a0d5b2ad8d9fd8b
Gauri Agrawal <73654083+Gauri-Agrawal838@users.noreply.github.com>
2023-04-11 09:24:12 +0530

    Time: 0 ms (100.00%), Space: 49.8 MB (39.23%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.java
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -1,11 +1,26 @@
 class Solution {
     public int hIndex(int[] citations) {
         int n = citations.length;
-        for(int i=0;i<citations.length;i++)
+        int low=0, high=n;
+        
+        // for(int i=0;i<citations.length;i++)
+        // {
+        //     if(citations[i]>=n-i)
+        //         return n-i;
+        // }
+        
+        int ans = 0;
+        while(low<high)
         {
-            if(citations[i]>=n-i)
-                return n-i;
+            int mid = low + (high-low)/2;
+            if(citations[mid]>=n-mid)
+            {
+                ans = Math.max(ans,n-mid);
+                high = mid;
+            }
+            else 
+                low = mid+1; 
         }
-        return 0;
+        return ans;
     }
 }
