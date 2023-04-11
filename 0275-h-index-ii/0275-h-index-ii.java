c941ed48de4a32ca9af0b4b6661954c2e095262e
Gauri Agrawal <73654083+Gauri-Agrawal838@users.noreply.github.com>
2023-04-11 09:15:40 +0530

    Time: 1 ms (25.22%), Space: 50.1 MB (10.86%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        for(int i=0;i<citations.length;i++)
+        {
+            if(citations[i]>=n-i)
+                return n-i;
+        }
+        return 0;
+    }
+}
