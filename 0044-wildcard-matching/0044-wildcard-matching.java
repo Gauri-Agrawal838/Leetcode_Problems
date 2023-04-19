f4d3bfd98407ef6500b25e4f4e9bfa09e76a84fa
Gauri Agrawal <73654083+Gauri-Agrawal838@users.noreply.github.com>
2023-04-19 09:18:06 +0530

    Time: 14 ms (85.74%), Space: 43.4 MB (44.18%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,34 +1,41 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int i=0, j=0;
-        int startIdx = -1, lastMatch = -1;
-        while(i<s.length())
+        char[] str = s.toCharArray();
+        char[] pattern = p.toCharArray();
+        int writeIndex = 0;
+        boolean isFirst = true;
+        for(int i=0;i<pattern.length;i++)
         {
-            if(j<p.length()&&(s.charAt(i)==p.charAt(j)||p.charAt(j)=='?'))
+            if(pattern[i]=='*')
             {
-                i++;
-                j++;
-            }
-            else if(j<p.length() && p.charAt(j)=='*')
-            {
-                startIdx = j;
-                lastMatch = i;
-                j++;
-            }
-            else if(startIdx!=-1)
-            {
-                j=startIdx+1;
-                lastMatch++;
-                i = lastMatch;
+                if(isFirst)
+                {
+                    pattern[writeIndex++] = pattern[i];
+                    isFirst = false;
+                }
             }
             else
-                return false;
+            {
+                pattern[writeIndex++] = pattern[i];
+                isFirst = true;
+            }
         }
-        boolean isMatch = true;
-        while(j<p.length()&&p.charAt(j)=='*')
-            j++;
-        if(i!=s.length()||j!=p.length())
-            isMatch = false;
-        return isMatch;
+        boolean T[][] = new boolean[str.length+1][writeIndex+1];
+        if(writeIndex>0 && pattern[0]=='*')
+            T[0][1] = true;
+        T[0][0] = true;
+        for(int i=1;i<T.length;i++)
+        {
+            for(int j=1;j<T[0].length;j++)
+            {
+                if(pattern[j-1]=='?' || str[i-1]==pattern[j-1])
+                {
+                    T[i][j] = T[i-1][j-1];
+                }
+                else if(pattern[j-1]=='*')
+                    T[i][j] = T[i-1][j] || T[i][j-1];
+            }
+        }
+        return T[str.length][writeIndex];
     }
 }
